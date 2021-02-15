6f6f96352af72aec0e48422e15bd49696e9656a0
Andriy Zhyshkovych <andrew_mail_box@yahoo.com>
2021-02-15 13:31:34 +0200

    Classical Inheritance

---
+++ 02 OOP fundamentals/05_classical-inheritance.php
@@ -0,0 +1,49 @@
+<?php
+
+# Object-Oriented PHP #5 - Classical Inheritance
+# https://www.codewars.com/kata/object-oriented-php-number-5-classical-inheritance
+
+class Salesman extends Person
+{
+    public function __construct($name, $age)
+    {
+        parent::__construct($name, $age, "Salesman");
+    }
+
+    public function introduce()
+    {
+        return "Hello, my name is $this->name, don't forget to check out my products!";
+    }
+}
+
+class ComputerProgrammer extends Person
+{
+    public function __construct($name, $age)
+    {
+        parent::__construct($name, $age, "Computer Programmer");
+    }
+
+    public function describe_job()
+    {
+        return "I am currently working as a(n) $this->occupation, don't forget to check out my Codewars account ;)";
+    }
+}
+
+class WebDeveloper extends ComputerProgrammer
+{
+    public function __construct($name, $age)
+    {
+        parent::__construct($name, $age, "Web Developer");
+    }
+
+    public function describe_job()
+    {
+        return parent::describe_job() . "I am currently working as a(n) $this->occupation,
+         don't forget to check out my Codewars account ;) And don't forget to check on my website :D";
+    }
+
+    public function describe_website()
+    {
+        return "My professional world-class website is made from HTML, CSS, Javascript and PHP!";
+    }
+}
