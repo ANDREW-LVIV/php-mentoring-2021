a55b40a3532e637b3c0e8c3eb134e036e30dc415
Andriy Zhyshkovych <andrew_mail_box@yahoo.com>
2021-02-15 14:00:24 +0200

    Visibility

---
+++ 02 OOP fundamentals/06_visibility.php
@@ -0,0 +1,68 @@
+<?php
+
+# Object-Oriented PHP #6 - Visibility
+# https://www.codewars.com/kata/object-oriented-php-number-6-visibility
+
+class Person
+{
+    protected $name, $age, $occupation;
+
+    public function __construct($name, $age, $occupation)
+    {
+        if (!is_string($name)) {
+            throw new InvalidArgumentException("Name must be a string!");
+        }
+
+        if ($age < 0) {
+            throw new InvalidArgumentException("Age must be a non-negative integer!");
+        }
+
+        if (!is_string($occupation)) {
+            throw new InvalidArgumentException("Occupation must be a string!");
+        }
+        $this->name = $name;
+        $this->age = $age;
+        $this->occupation = $occupation;
+    }
+
+
+    public function get_name()
+    {
+        return $this->name;
+    }
+
+    public function get_age()
+    {
+        return $this->age;
+    }
+
+    public function get_occupation()
+    {
+        return $this->occupation;
+    }
+
+
+    public function set_name($name)
+    {
+        if (!is_string($name)) {
+            throw new InvalidArgumentException("Name must be a string!");
+        }
+        $this->name = $name;
+    }
+
+    public function set_age($age)
+    {
+        if ($age < 0) {
+            throw new InvalidArgumentException("Age must be a non-negative integer!");
+        }
+        $this->age = $age;
+    }
+
+    public function set_occupation($occupation)
+    {
+        if (!is_string($occupation)) {
+            throw new InvalidArgumentException("Occupation must be a string!");
+        }
+        $this->occupation = $occupation;
+    }
+}
