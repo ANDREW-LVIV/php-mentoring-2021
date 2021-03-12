01f02942ba17a38a85b76dd0dc65ebcfedb68c9a
Andriy Zhyshkovych <andrew_mail_box@yahoo.com>
2021-03-13 01:40:00 +0200

    Symfony Doctrine (draft)

---
+++ 04 Doctrine CRUD/src/Controller/GuestBookController.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Guestbook;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GuestBookController extends AbstractController
+{
+
+    /**
+     * @Route("/{page}", name="main", requirements={"page"="\d+"})
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param int $page
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request, $page = 1)
+    {
+        $order = $request->get('order') ?? 'ASC';
+
+        $previous_page = false;
+        $next_page = false;
+        $offset = 0;
+        $limit = 5;
+
+        if ($page > 1) {
+            $offset += $limit * ($page - 1);
+            $previous_page = $page - 1;
+        }
+
+        $repository = $this->getDoctrine()->getRepository(Guestbook::class);
+        $records = $repository->findBy(
+          [],
+          ['id' => $order],
+          $limit + 1,
+          $offset
+        );
+
+        if (count($records) > $limit) {
+            $next_page = $page + 1;
+            array_pop($records);
+        }
+
+        return $this->render(
+          'index.html.twig',
+          [
+            'order' => $order,
+            'page' => $page,
+            'previous_page' => $previous_page,
+            'next_page' => $next_page,
+            'records' => $records,
+          ]
+        );
+    }
+
+    /**
+     * @Route("/search", name="search_page")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return Response
+     */
+    public function search(
+      Request $request,
+      EntityManagerInterface $entityManager
+    ): Response {
+        $word = $request->get('word');
+        $repository = $this->getDoctrine()->getRepository(Guestbook::class);
+        $records = $repository->findByMsgField($word);
+        return $this->render(
+          'search.html.twig',
+          [
+            'word' => $word,
+            'records' => $records,
+          ]
+        );
+    }
+
+    /**
+     * @Route("/task", name="task")
+     */
+    public function task()
+    {
+        return $this->render('task.html.twig');
+    }
+
+    /**
+     * @Route("/add", name="add", methods="POST")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function create(
+      Request $request,
+      EntityManagerInterface $entityManager
+    ) {
+        $record = new Guestbook();
+        $record->setNick($request->get('nick'))
+          ->setMsg($request->get('msg'))
+          ->setTimestamp(new DateTime("now"));
+        $entityManager->persist($record);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @param int $id
+     */
+    public function edit(int $id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $record = $entityManager->getRepository(Guestbook::class)->find($id);
+
+        return $this->render(
+          'edit.html.twig',
+          [
+            'record' => $record,
+          ]
+        );
+    }
+
+    /**
+     * @Route("/update/{id}", name="update", methods="POST")
+     * @param $id
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function update(
+      $id,
+      Request $request,
+      EntityManagerInterface $entityManager
+    ) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $record = $entityManager->getRepository(Guestbook::class)->find($id);
+        if (!$record) {
+            throw $this->createNotFoundException(
+              'No record found for id ' . $id
+            );
+        }
+
+        $record->setNick($request->get('nick'))
+          ->setMsg($request->get('msg'));
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param int $id
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function delete(int $id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $record = $entityManager->getRepository(Guestbook::class)->find($id);
+        $entityManager->remove($record);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
+
+}
---
+++ 04 Doctrine CRUD/src/Entity/Guestbook.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GuestbookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GuestbookRepository::class)
+ * @ORM\Table(name="gb_records")
+ */
+class Guestbook
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nick;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $msg;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $timestamp;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNick(): ?string
+    {
+        return $this->nick;
+    }
+
+    public function setNick(string $nick): self
+    {
+        $this->nick = $nick;
+
+        return $this;
+    }
+
+    public function getMsg(): ?string
+    {
+        return $this->msg;
+    }
+
+    public function setMsg(string $msg): self
+    {
+        $this->msg = $msg;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?\DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(\DateTimeInterface $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+}
